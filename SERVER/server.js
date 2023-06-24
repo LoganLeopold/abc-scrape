7133e8adf9bdfbf5d85e8935e42d58ef5ffee205
Logan Leopold <loganleopold@me.com>
2023-06-24 15:18:19 -0400

    Working url on dummydata with wypnts + wypnt plcids

--- SERVER/server.js
+++ SERVER/server.js
@@ -41,7 +41,7 @@ const createGooglePlaces = async (addresses) => {
           key: process.env.GOOGLE_MAPS_KEY,
           input: `${add} Virginia ABC`,
           inputtype: "textquery",
-          fields: ['geometry']
+          fields: ['geometry', 'place_id']
         }
       })
       return thisPlace.data
@@ -54,36 +54,44 @@ const createGooglePlaces = async (addresses) => {
 
 const dateFilterPlaces = (placeArr) => {
   const homeLocation = {lat: 38.885474, lon: -76.992642} 
+  // Find the close places
   const closePlaces = placeArr.reduce((acc, curr, i) => {
+    // if (i == 0 && curr.status === "OK") console.log(curr.candidates[0])
     if (curr.candidates.length > 0) {
       let { lat, lng } = curr.candidates[0].geometry.location
       let miles = Distance.between(
         homeLocation,
         {lat: lat, lon: lng}
       ).human_readable('customary').distance
-      if (Number(miles) < 10) acc.push([miles, [lat, lng]]);
+      if (Number(miles) < 10) acc.push([miles, curr]);
     }
     return acc
   },[])
   
+  // sort them by distance and return just the place object
   const distanceSortedPlaces = closePlaces.sort((a,b) => a[0] - b[0]).map(place => place[1]);
 
-  
-  const destination = encodeURIComponent(distanceSortedPlaces[distanceSortedPlaces.length - 1]);
+  const { 
+    geometry: {location: { lat: destinationLat, lng: destinationLng }}, place_id: destinationPlaceId 
+  } = distanceSortedPlaces[distanceSortedPlaces.length - 1].candidates[0];
+  const urlDestination = `${destinationLat},${destinationLng}`
 
-  const waypoints = distanceSortedPlaces.reduce((acc, curr, i, arr) => {
-    [lat, long] = curr;
-    // if (i < arr.length - 2) {
-      acc += `${lat},${long}${i < arr.length - 1 ? '|' : ''}`;
-    // }
-    return acc;
-  }, '')
+  let waypoints = '';
+  let waypointIds = '';
 
-  const encodedWaypoints = encodeURIComponent(waypoints);
+  for (i = 0; i < distanceSortedPlaces.length - 1; i++) {
+    const curr = distanceSortedPlaces[i];
+    console.log(curr.candidates[0].place_id)
+    const {lat, lng} = curr.candidates[0].geometry.location;
+    console.log(lat, lng)
+    waypoints += `${lat},${lng}${i < distanceSortedPlaces.length - 2 ? '%7C' : ''}`;
+    waypointIds += `${curr.candidates[0].place_id}${i < distanceSortedPlaces.length - 2 ? '%7C' : ''}`
+  }
 
-  const url = `https://www.google.com/maps/dir/?api=1&waypoints=${encodedWaypoints}`;
+  const url = `https://www.google.com/maps/dir/?api=1&waypoints=${waypoints}&waypoint_place_ids=${waypointIds}&destination=${urlDestination}&destination_place_id=${destinationPlaceId}`;
 
-  console.log(url)
+  console.log(url);
+
   return distanceSortedPlaces;
 }
 
@@ -96,10 +104,13 @@ app.get('/processLocations', async (req, res) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   console.log("processLocations")
   try {
-    // let addresses = await parseList('https://www.abc.virginia.gov/limited/allocated_stores_02_06_2023_02_30_pmlhHUeqm1xIf7QPX8FDXhde8V.html')
-    // let places = await createGooglePlaces(addresses)
-
-    const closePlaces = dateFilterPlaces(dummyData);
+    let addresses = await parseList('https://www.abc.virginia.gov/limited/allocated_stores_02_06_2023_02_30_pmlhHUeqm1xIf7QPX8FDXhde8V.html')
+    let places = await createGooglePlaces(addresses)
+    
+    // for testing distance filter
+      // const closePlaces = dateFilterPlaces(dummyData);
+    // for actual google places
+      const closePlaces = dateFilterPlaces(places)
    
     res.json(closePlaces)
   } catch (err) {
