41ee9aac20e2222a4a902dedeb6005e98a2c78d9
Logan Leopold <loganleopold@me.com>
2023-09-15 15:14:03 -0400

    Decent geolocation button look

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [currentLocation, setCurrentLocation] = useState('')
   const [currentCoords, setCurrentCoords] = useState({})
   const [submitDisabled, setSubmitDisabled] = useState(true)
-  const [locationMethod, setLocationMethod] = useState('init')
+  const [locationMethod, setLocationMethod] = useState('')
   const formRef = useRef(null)
 
   const errorMessages = {
@@ -19,7 +19,6 @@ function App() {
   }
 
   const geoLocateSuccess = (data) => {
-    console.log(data.coords)
     const { latitude, longitude } = data.coords
     setCurrentCoords({lat: latitude, lng: longitude})
     setSubmitDisabled(false)
@@ -27,7 +26,6 @@ function App() {
   }
 
   const geoLocateError = (error) => {
-    console.log(error)
     setSubmitDisabled(false)
   }
 
@@ -49,10 +47,6 @@ function App() {
     setDropUrl(e.target.value)
   }
 
-  // const validateFields = () => {
-  //   return dropUrl && (currentCoords.lat !== 0 || currentLocation)
-  // }
-
   const processLocations = async (e) => {
     e.preventDefault()
     formRef.current.reportValidity()
@@ -67,7 +61,7 @@ function App() {
     } else if (currentLocation.length > 0) {
       payload['currentLocation'] = currentLocation
     } else {
-      setLocationMethod('init')
+      setLocationMethod('')
       return 
     }
 
@@ -101,8 +95,8 @@ function App() {
           <input className="form_input" type="text" name="drop_url" placeholder='Drop Url Goes Here' onChange={onUrlInputChange} htmlFor="link_submit" required/>
         </div>
 
-        <h2>2. Define search area using:</h2>
-        {locationMethod === "init" &&
+        <h2>2. Define search area using: {locationMethod}</h2>
+        {locationMethod === '' &&
           <div className='location_method'>
             <button onClick={() => setLocationMethod('written')}>A Typed City, State</button>
             <button onClick={() => setLocationMethod('geolocation')}>Your Current Location</button>
@@ -113,15 +107,26 @@ function App() {
           {locationMethod === 'written' && (
             <>
               <input className="form_input" type="text" name="current_location" placeholder='City, State' onChange={onCurrentLocationChange} htmlFor="link_submit" disabled={locationMethod !== 'written'} required={locationMethod === 'written'}/>
-              <button onClick={() => setLocationMethod('init')} className='back'>{'< Pick a different method'}</button>
+       
+              <button onClick={() => setLocationMethod('')} className='back'>{'< Pick a different method'}</button>
             </>
           )}
         </div>
         { locationMethod === 'geolocation' && 
           <>
-            {Object.entries(currentCoords).length === 0 && <button type="submit" value="Fetch Location >" onClick={onGeoLocation} htmlFor="location_fetch" disabled={locationMethod !== 'geolocation'} required={locationMethod === 'geolocation'}>Fetch Location</button>}
-            {Object.entries(currentCoords).length > 0 && <h3>{`Lat: ${currentCoords.lng} Lng: ${currentCoords.lng}`}</h3>}
-            <button onClick={() => setLocationMethod('init')} className='back'>{'< Pick a different method'}</button>
+            {Object.entries(currentCoords).length === 0 && 
+              <button type="submit" value="Fetch Location >" onClick={onGeoLocation} htmlFor="location_fetch" disabled={locationMethod !== 'geolocation'} required={locationMethod === 'geolocation'}>Fetch Location</button>
+            }
+            {Object.entries(currentCoords).length > 0 && 
+              <div className="geolocations">
+                <label>Latitude:</label>
+                <input className="form_input geo" name="lat" value={currentCoords.lat} required/>
+                <label>Longitude</label>
+                <input className="form_input geo" name="lng" value={currentCoords.lng} required/>
+                <button onClick={() => setCurrentCoords('')}>{'Reset Location'}</button>
+              </div>
+            }
+            <button onClick={() => setLocationMethod('')} className='back'>{'< Pick a different method'}</button>
           </>
         }
 
