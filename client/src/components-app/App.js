2ea8d91d4f2167a289ab22391e3dacef3b6ff80e
Logan Leopold <loganleopold@me.com>
2024-07-01 15:54:08 -0400

    client .env for post req url

--- client/src/components-app/App.js
+++ client/src/components-app/App.js
@@ -51,10 +51,14 @@ const App = () => {
       return 
     }
 
+    const server = process.env.REACT_APP_ENV === 'dev' ? 'http://localhost:3001/processLocations' : '/api/processLocations' 
+    console.log(server, process.env.REACT_APP_ENV)
+
     try {
       const response = await axios.post(
+        server,
         // '/api/processLocations', 
-        'http://localhost:3001/processLocations',
+        // 'http://localhost:3001/processLocations',
         payload,
         {timeout: 15000},
       );
