2d6111a2afcf7b56617f0eb37347f0bd1fd4bb42
Logan Leopold <loganleopold@me.com>
2024-07-08 18:07:13 -0400

    Some better lookin stuff I think

--- client/src/components-app/App.tsx
+++ client/src/components-app/App.tsx
@@ -126,7 +126,7 @@ const App = () => {
       <form id="link_submit" ref={formRef}>
         <div className='input_group'>
           <h2>1. Place the latest drop url.</h2>
-          <input className="form_input" type="text" name="drop_url" placeholder='' onChange={onUrlInputChange} required value={dropUrl}/>
+          <input className="drop-url" type="text" name="drop_url" placeholder='' onChange={onUrlInputChange} required value={dropUrl}/>
         </div>
 
         {dropUrl &&
@@ -138,7 +138,7 @@ const App = () => {
 
         { dropUrl && currentLocation[currentLocationMethod as keyof ProcessStateObject] && currentState !== 'error' &&
           <div className={`input_group fadeIn`}>
-            <h2>3. Get closest stores as waypoints.</h2>
+            <h2>3. Get closest stores.</h2>
             <button
               type="submit" 
               className={`${currentState !== 'initialized' ? 'cursor-disable' : ''}`}
--- client/src/components-app/Results.tsx
+++ client/src/components-app/Results.tsx
@@ -41,12 +41,12 @@ const Results: React.FC<PropsWithChildren<Props>> = (props: Props) => {
           Circuit Link
         </button>
       </div>
-      <p className={`${selected === 'waypoints' ? 'active' : ''}`}>This creates a link that routes you to up to 10 closest locations as a circuit. You may need to rearrange the order for an optimal route. </p>
       <div className={`result-option ${selected === 'waypoints' ? 'active' : ''}`}>
-        <a href={finalWaypoints} target='_blank' rel="noreferrer">{'Open In Maps >'}</a>
+        <p>This creates a link that routes you to up to 10 closest locations as a circuit. You may need to rearrange the order for an optimal route. </p>
+        <a href={finalWaypoints} className='waypoints' target='_blank' rel="noreferrer">{'Open In Maps >'}</a>
       </div>
-      <p className={`${selected === 'list' ? 'active' : ''}`}>A list of up to the top 10 closest locations.</p>
       <div className={`result-option ${selected === 'list' ? 'active' : ''}`}>
+        <p>A list of up to the top 10 closest locations.</p>
         <div className='links'>
           { 
             individualLinks.length > 0 && individualLinks.map( link => 
